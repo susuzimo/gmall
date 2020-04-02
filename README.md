6831d09d5619da64b419dd5e3640eeee88933348
âsusuzimo <1432625686@qq.com>
2020-04-02 21:00:40 +0800

    2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # gmall0105 本地修改版本
 
 
-gmall-user  8080
+#gmall-user  8080
 gmall-user-service层用户服务 8070
 gmall-user-web层用户服务 8080
 
--- gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -5,7 +5,6 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.UmsMember;
 import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
